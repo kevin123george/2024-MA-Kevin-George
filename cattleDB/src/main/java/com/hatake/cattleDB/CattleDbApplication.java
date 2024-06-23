320e572b6661fb955ea387260ec4688b25c89c80
Kevin <kevin.george@stud.uni-bamberg.de>
2024-06-23 10:05:50 +0200

    cattle db

---
+++ cattleDB/src/main/java/com/hatake/cattleDB/CattleDbApplication.java
@@ -0,0 +1,13 @@
+package com.hatake.cattleDB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CattleDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CattleDbApplication.class, args);
+	}
+
+}
